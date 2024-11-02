e8cb0f9772c36caec2d7da196cd5f913a9f9d518
kapil <kyadav4@wisc.edu>
2024-11-01 20:31:37 -0400

    Adding TechStack Entires

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -4,7 +4,7 @@ import { CgCPlusPlus } from "react-icons/cg";
 import { SiTableau } from "react-icons/si";
 import { FaEmber } from "react-icons/fa";
 import { FaHtml5 } from "react-icons/fa";
-import { SiMysql } from "react-icons/si";
+import { SiMysql,SiCss3  } from "react-icons/si";
 import {
   DiJavascript1,
   DiReact,
@@ -13,6 +13,8 @@ import {
   DiPython,
   DiGit,
   DiJava,
+  DiAngularSimple,
+  DiNodejsSmall, 
 } from "react-icons/di";
 import {
   SiRedis,
@@ -28,38 +30,62 @@ function Techstack() {
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
       <Col xs={4} md={2} className="tech-icons">
         <DiJavascript1 />
+        <p className="tech-label">JavaScript</p>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiReact />
+        <p className="tech-label">React</p>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <FaEmber  />
+        <DiAngularSimple />
+        <p className="tech-label">Angular</p>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <SiTableau  />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiGit />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <FaHtml5  />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiRedis />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiPostgresql />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiPython />
+        <FaEmber />
+        <p className="tech-label">Ember.js</p>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiJava />
+        <p className="tech-label">Java</p>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <SiMysql  />
+        <DiNodejsSmall />
+        <p className="tech-label">Node.js</p>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiMysql />
+        <p className="tech-label">MySQL</p>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiPython />
+        <p className="tech-label">Python</p>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <FaHtml5 />
+        <p className="tech-label">HTML5</p>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+  <SiCss3 />
+  <p className="tech-label">CSS</p>
+</Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiTableau />
+        <p className="tech-label">Tableau</p>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiGit />
+        <p className="tech-label">Git</p>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiRedis />
+        <p className="tech-label">Redis</p>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiPostgresql />
+        <p className="tech-label">PostgreSQL</p>
       </Col>
     </Row>
+
   );
 }
 
